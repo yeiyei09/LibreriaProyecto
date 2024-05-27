ae85eff03a719e0d5aac99ea7de71f34aba78245
yeiyei09 <yeffergomez02@gmail.com>
2024-05-27 09:44:22 -0500

    yeffer crear clase y atributos

--- src/Libreria.java
+++ src/Libreria.java
@@ -1,5 +1,5 @@
 public class Libreria {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("AQUI VA EL PROYECTO");
     }
 }
---
+++ src/Libro.java
@@ -0,0 +1,16 @@
+
+public class Libro {
+    String titulo, nombreA, apellidoA, editorial, isbn, fecha;
+    int precio;
+
+    public Libro(String titulo, String nombreA, String apellidoA, String editorial, String isbn, String fecha, int precio){
+        this.titulo = titulo;
+        this.nombreA = nombreA;
+        this.apellidoA = apellidoA;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
+    
+}
