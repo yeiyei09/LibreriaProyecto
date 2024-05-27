e445d0fac239c79c5db6a2b0bb10b3e4852f5126
yeiyei09 <yeffergomez02@gmail.com>
2024-05-27 10:28:32 -0500

    yefferson cambio clase

--- src/Libro.java
+++ src/Libro.java
@@ -1,9 +1,9 @@
 
 public class Libro {
     String titulo, nombreA, apellidoA, editorial, isbn, fecha;
-    int precio;
+    int precio, vendidos;
 
-    public Libro(String titulo, String nombreA, String apellidoA, String editorial, String isbn, String fecha, int precio){
+    public Libro(String titulo, String nombreA, String apellidoA, String editorial, String isbn, String fecha, int precio, int vendidos){
         this.titulo = titulo;
         this.nombreA = nombreA;
         this.apellidoA = apellidoA;
@@ -11,10 +11,14 @@ public class Libro {
         this.isbn = isbn;
         this.fecha = fecha;
         this.precio = precio;
+        this.vendidos = vendidos;
     }
 
-    public void mostrarLibros(int decision, Libro[] libro){
-
+    public void librosVendidos(int decision, Libro[] libro){
+        for(int i = 0; i < libro.length; i++){
+            System.out.println("Libro: " + libro[i].titulo);
+            System.out.println("Cantidad vendidos hoy: " + libro[i].vendidos);
+        }
     }
     
 }
