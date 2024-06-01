7ff71decb942035632d5e6511a99a2e3db51561e
yeiyei09 <yeffergomez02@gmail.com>
2024-06-01 17:38:20 -0500

    yefferson metodos eliminarLibro, venderLibros

--- src/Inventario.java
+++ src/Inventario.java
@@ -8,10 +8,10 @@ public class Inventario {
         String menor;
         Libro aux;
         
-        for (int i = 0; i < libro.length; i++) {
+        for (int i = 0; i < Libreria.librosR; i++) {
             menor = libro[i].titulo;
             mn = i;
-            for (int j = i + 1; j < libro.length; j++) {
+            for (int j = i + 1; j < Libreria.librosR; j++) {
                 if(menor.compareTo(libro[j].titulo)>0){
                     mn = j;
                     
@@ -27,7 +27,7 @@ public class Inventario {
     public void agg(Libro[]libro){
         boolean salir = true;
         String tit, nomA, apllA, editorial, isbn, fechaC, opcion;
-        int precio = 0, vendidosDia, ejemplar = 0;
+        int precio = 0, vendidosDia, ejemplar = 0, estado = 0;
         while(salir){
             boolean existe = false, valido = true;
             System.out.println("Ingrese los datos del libro");
@@ -36,10 +36,6 @@ public class Inventario {
             System.out.println("Ingrese el titulo del libro");
             tit = sc.next();
             
-            
-            System.out.println("Ingrese el nombre del autor de : "+tit);
-            nomA = sc.next();
-               
             System.out.println("Ingrese el nombre del autor de : "+tit);
             nomA = sc.next();
             
@@ -76,6 +72,19 @@ public class Inventario {
                     sc.next();
                 }
             }
+            valido = true;
+            while(valido){
+                System.out.println("Ingrese el estado de "+tit+". 1 Si el libro se puede reimprimir, 2 si no se puede reimprimir");
+                try {
+                    estado = sc.nextInt();
+                    if(estado == 1 || estado == 2){
+                        valido = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Datos no válidos, ingrese de nuevo");
+                    sc.next();
+                }
+            }
 
             vendidosDia = 0;
 
@@ -90,19 +99,82 @@ public class Inventario {
             }
             if(!existe){
                 if(Libreria.librosR < Libreria.lib.length){
-                    libro[Libreria.librosR] = new Libro(tit, nomA, apllA, editorial, isbn, fechaC, precio, vendidosDia, ejemplar);
+                    libro[Libreria.librosR] = new Libro(tit, nomA, apllA, editorial, isbn, fechaC, precio, vendidosDia, ejemplar, estado);
                     Libreria.librosR ++;
                 }else{
                     System.out.println("NO HAY MAS ESPACIOS PARA LIBROS, VERIFIQUE EL ISBN SI QUIERE AGREGAR CANTIDADES EXISTENTES");
                 }
             }
- 
 
             System.out.println("¿Desea agregar otro libro?");
             opcion = sc.next();
             if(!opcion.equalsIgnoreCase("si"))
-                salir = false;
-            
+                salir = false;    
+        }
+    }
+
+    //eliminar libros automaticamente
+    public void eliminarLibros(Libro[]libro){
+        for(int i = 0; i<Libreria.librosR; i++){
+            if(libro[i].ejemplares==0 && libro[i].estado==2){
+                System.out.println("El libro "+libro[i].titulo+" sera eliminado, no mas existencias disponibles");
+                for(int j = i; j<Libreria.librosR-1; j++){
+                    libro[j] = libro[j+1];
+                }
+                libro[Libreria.librosR-1] = null;
+                Libreria.librosR -= 1;
+            }
+        }
+    }
+
+    //venta de libros
+    public void venderLibros(Libro[]libro){
+        String comprar, decision;
+        int unidades, i;
+        boolean comprobar = true, encontrado = false;
+
+        while(comprobar){
+            System.out.println("Ingrese el titulo del libro que desea vender: ");
+            comprar = sc.next();
+            for(i = 0; i<Libreria.librosR; i++){
+                if(comprar.equalsIgnoreCase(libro[i].titulo)){
+                    encontrado = true;
+                    break;
+                }
+            }
+            while(encontrado){
+                System.out.println("Ingrese la cantidad de unidades del libro que desea vender");
+                unidades = sc.nextInt();
+                if(unidades>libro[i].ejemplares && libro[i].estado==2){
+                    System.out.println("La cantidad que se quiere vender excede las unidades disponibles");
+                    System.out.println("Se quiere vender "+unidades);
+                    System.out.println("Hay disponibles "+ libro[i].ejemplares);
+                    System.out.println("Intentelo de nuevo");
+                }else{
+                    if(unidades>libro[i].ejemplares && libro[i].estado==1){
+                        int aux = libro[i].ejemplares;
+                        libro[i].ejemplares -= unidades;
+                        if(libro[i].ejemplares<0){
+                            System.out.println("Esta venta excede las unidades disponibles en inventario, pero es reimprimible, por lo que se deben de pedir "+(-1*libro[i].ejemplares)+" unidades a la editorial "+libro[i].editorial);
+                            System.out.println("Se pudieron vender al instante "+ aux+" unidades");
+                        }
+                    }else{
+                        if(unidades<=libro[i].ejemplares){
+                            libro[i].ejemplares -= unidades;
+                            System.out.println("Se vendieron "+unidades+" del libro "+libro[i].titulo);
+                        }
+                    }                
+                }
+                System.out.println("¿Desea vender otro libro?");{
+                    decision = sc.next();
+                    if(decision.equalsIgnoreCase("si")){
+                        break;
+                    }else{
+                        comprobar = false;
+                        break;
+                    }
+                }
+            }
         }
 
     }
