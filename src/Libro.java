c1fc6d2dbfd0f7facfa2374276dfc219903328a0
yeiyei09 <yeffergomez02@gmail.com>
2024-06-02 14:37:20 -0500

    yefferson Libro finalizado

--- src/Libro.java
+++ src/Libro.java
@@ -1,9 +1,9 @@
 
 public class Libro {
     String titulo, nombreA, apellidoA, editorial, ISBN, fechaC;
-    int precio, vendidosDia, ejemplares;
+    int precio, vendidosDia, ejemplares, estado;
 
-    public Libro(String titulo, String nombreA, String apellidoA, String editorial, String ISBN, String fechaC, int precio, int vendidosDia, int ejemplares){
+    public Libro(String titulo, String nombreA, String apellidoA, String editorial, String ISBN, String fechaC, int precio, int vendidosDia, int ejemplares, int estado){
         this.titulo = titulo;
         this.nombreA = nombreA;
         this.apellidoA = apellidoA;
@@ -13,20 +13,6 @@ public class Libro {
         this.precio = precio;
         this.vendidosDia = vendidosDia;
         this.ejemplares = ejemplares;
+        this.estado = estado;  
     }
-    //Metodo para ordenar alfabeticamente
-    
-    //MetodoS para la RN 1
-
-
-    //Metodo para la RN 4
-    public void librosVendidos(int decision, Libro[] libro){
-        for(int i = 0; i < libro.length; i++){
-            System.out.println("Libro: " + libro[i].titulo);
-            System.out.println("Cantidad vendidos hoy: " + libro[i].vendidosDia);
-        }
-    }
-
-
-    
 }
